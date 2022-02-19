d31f88c3abac0ce75ab8929592d7ca9bb5384d8c
Your Name <nthaninus@gmail.com>
2022-02-19 18:11:44 -0500

    delete comment

--- player.cpp
+++ player.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     struct addrinfo *host_info_list;
     const char *hostname = argv[1]; // 152.3.76.77
     const char *port = argv[2];     // 4444
-    srand (time(NULL));
+    srand(time(NULL));
     memset(&host_info, 0, sizeof(host_info));
     host_info.ai_family = AF_UNSPEC;
     host_info.ai_socktype = SOCK_STREAM;
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
         std::cerr << "Error: cannot get address info for host" << std::endl;
         std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
         return -1;
-    } // if
+    }
 
     socket_fd = socket(host_info_list->ai_family,
                        host_info_list->ai_socktype,
@@ -49,10 +49,7 @@ int main(int argc, char *argv[])
         std::cerr << "Error: cannot create socket" << std::endl;
         std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
         return -1;
-    } // if
-
-    //std::cout << "Connecting to " << hostname << " on port " << port << "..." << std::endl;
-
+    }
     status = connect(socket_fd, host_info_list->ai_addr, host_info_list->ai_addrlen);
     if (status == -1)
     {
@@ -66,15 +63,10 @@ int main(int argc, char *argv[])
     const char *message = "hi there!";
     Potato play_potato;
     Player myInfo;
-    // send(socket_fd, message, strlen(message), 0);
     send(socket_fd, &myInfo, sizeof(myInfo), 0);
     recv(socket_fd, &myInfo, sizeof(myInfo), 0);
-    //std::cout << "Myport: " << myInfo.my_port << std::endl;
-    //std::cout << "My left: " << myInfo.my_left << std::endl;
     std::cout << "Connected as player " << myInfo.seqNo << " out of " << myInfo.num_players << " total players" << std::endl;
     status = recv(socket_fd, &play_potato, sizeof(play_potato), 0);
-    // if (status > 0){
-    
     while (play_potato.num_hops > 0)
     {
         if (status <= 0)
@@ -83,18 +75,21 @@ int main(int argc, char *argv[])
         }
         play_potato.num_hops--;
         play_potato.count++;
-        //std::cout << "hops " << play_potato.num_hops << std::endl;
+        // std::cout << "hops " << play_potato.num_hops << std::endl;
         int leftOrRight = rand();
-        //std::cout << leftOrRight << std::endl;
-        if (leftOrRight % 2 == 0){
+        // std::cout << leftOrRight << std::endl;
+        if (leftOrRight % 2 == 0)
+        {
             leftOrRight = myInfo.my_right;
-        } else {
+        }
+        else
+        {
             leftOrRight = myInfo.my_left;
         }
         play_potato.game_progress[play_potato.count] = leftOrRight;
         if (play_potato.num_hops > 0)
         {
-            std::cout << "sending potato to " << leftOrRight<< std::endl;
+            std::cout << "sending potato to " << leftOrRight << std::endl;
         }
         send(socket_fd, &play_potato, sizeof(play_potato), 0);
         if (play_potato.num_hops == 0)
@@ -103,7 +98,6 @@ int main(int argc, char *argv[])
         }
         status = recv(socket_fd, &play_potato, sizeof(play_potato), 0);
     }
-    //}
     freeaddrinfo(host_info_list);
     close(socket_fd);
     return 0;
--- ringmaster.cpp
+++ ringmaster.cpp
@@ -42,7 +42,6 @@ int main(int argc, char *argv[])
     std::vector<Player> players_vec;
     int players_connection_fd;
     std::string playerID;
-    // char buffer[512];
     Player player_info;
     while (players_vec.size() < num_players)
     {
@@ -50,7 +49,6 @@ int main(int argc, char *argv[])
         players_connection_fd = accept(socket_fd, (struct sockaddr *)&socket_addr, &socket_addr_len);
         struct sockaddr_in *addr = (struct sockaddr_in *)&socket_addr;
         std::string ip(inet_ntoa(addr->sin_addr));
-        // std::cout << ip <<" "<< addr->sin_port << std::endl;
         if (players_connection_fd == -1)
         {
             std::cerr << "Error: cannot accept connection on socket" << std::endl;
@@ -61,16 +59,12 @@ int main(int argc, char *argv[])
         {
             player_info.my_fd = players_connection_fd;
             player_info.seqNo = players_vec.size();
-            // player_info.my_host= "fuck";
             player_info.my_port = addr->sin_port;
             players_vec.push_back(player_info);
             std::cout << "player " << players_vec.size() - 1 << " is ready to play" << std::endl;
         }
     }
 
-    // buffer[9] = 0;
-
-    // std::cout << "Server send: " << received_potato.game_progress << std::endl;
     for (int i = 0; i < players_vec.size(); i++)
     {
         players_vec[i].num_players =  players_vec.size();
@@ -78,9 +72,7 @@ int main(int argc, char *argv[])
         int right_fd_index = (i + 1 + players_vec.size()) % players_vec.size();
         players_vec[i].my_left = players_vec[left_fd_index].seqNo;
         players_vec[i].my_right = players_vec[right_fd_index].seqNo;
-        // players_vec[i].nei_host.push_back(players_vec[right_fd_index].my_host[0]);
         send(players_vec[i].my_fd, &players_vec[i], sizeof(players_vec[i]), 0);
-        // std::cout << players_vec[i].my_fd << std::endl;
     }
     Potato sent_potato;
     sent_potato.num_hops = num_hops + 1;
@@ -97,17 +89,12 @@ int main(int argc, char *argv[])
     {
         play_path.push_back(send_to_player);
         recv(players_vec[send_to_player].my_fd, &sent_potato, sizeof(sent_potato), 0);
-        // std::cout << "receiv potato from: " << send_to_player << std::endl;
-        // std::cout << "num_hop: " << sent_potato.num_hops << std::endl;
         send_to_player = sent_potato.game_progress[sent_potato.count];
         if (sent_potato.num_hops <= 0)
         {
-            // send(players_vec[send_to_player].my_fd, &terminator_potato, sizeof(sent_potato), 0);
-            // std::cout << "Game over " <<std::endl ;
             break;
         }
         send(players_vec[send_to_player].my_fd, &sent_potato, sizeof(sent_potato), 0);
-        // std::cout << "Server send potato to: " << send_to_player << std::endl;
     }
     std::cout << "Trace of Potato:" << std::endl;
     for (int i = 0; i < sent_potato.count; i++)
